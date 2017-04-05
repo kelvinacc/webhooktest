ab7c10a9f0afe37eec35237c8003665907628d4a
Kelvin Musalula <kelvin.musalula@accenture.com>
2017-04-05 17:30:26 +0100

    change

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ restService.use(bodyParser.json());
 restService.post('/', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again.";
     console.log(req.body.result);
-    if (req.body.result.metadata == null) {
+
+    if (req.body.result.metadata === null) {
         return res.json({
             speech: speech,
             displayText: speech,
