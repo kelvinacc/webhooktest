9bc6d9e76723f20833b5de21db082dfe66fe1f10
Kelvin Musalula <kelvin.musalula@accenture.com>
2017-04-06 10:46:41 +0100

    Siginificant

--- index.js
+++ index.js
@@ -15,11 +15,38 @@ restService.post('/', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again.";
     console.log("These are the results:" + req.body.result);
 
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-sample'
-    });
+    if (req.body.result.metadata.intentName == "GoBack") {
+        speech = "Going back dude";
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'webhook-sample'
+        });
+    }
+    else if (req.body.result.metadata.intentName == "Next") {
+        speech = "Forward it is";
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'webhook-sample'
+        });
+    }
+    else if (req.body.result.metadata.intentName == "Play") {
+        speech = "Playing nothing now, enjoy the nothingness";
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'webhook-sample'
+        });
+    }
+    else{
+        speech = "You know what, I just don't get you";
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'webhook-sample'
+        });
+    }
 
 });
 
